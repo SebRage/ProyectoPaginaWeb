c5509c9854bf004b9ac9b9e3dc94bc756ef280ac
SebRage <139822791+SebRage@users.noreply.github.com>
2023-09-28 23:40:15 -0500

    Versión 3

--- Proyecto Pagina Web/js/app.js
+++ Proyecto Pagina Web/js/app.js
@@ -10,24 +10,23 @@ const formValid = {
     telefono: false
 };
 //envio formulario
-enviar.addEventListener("click", function (e){
+enviar.addEventListener("click", function (e) {
     e.preventDefault();//no se hace el envio del fomulario por lo tanto no se refresca
     console.log(Object.values(formValid));
-    if(formValidValues(formValid)=== -1){
+    if (formValidValues(formValid) === -1) {
         alert("Enviando fomulario")
-    }else{
+    } else {
         alert("Campos invalidos")
     }
 });
-const formValidValues = (Objeto)=>{
+const formValidValues = (Objeto) => {
     const valores = Object.values(Objeto);
-    let response = valores.findIndex((e) =>e === false) ;
+    let response = valores.findIndex((e) => e === false);
     return response;
-
-    
 }
+
 //validacion a traves del cambio de los elementos del fomulario 
-form.addEventListener("change",(e)=>{
+form.addEventListener("change", (e) => {
     const inputId = e.target.id;
     console.log(inputId);
     const myValue = e.target.value;
@@ -35,35 +34,33 @@ form.addEventListener("change",(e)=>{
     const myClass = e.target.classList;
     console.log(myClass);
     //Funciones que agragan o quita los estilos validos e invalidos
-    const validClass = ()=>{
+    const validClass = () => {
         myClass.add("is-valid");
         myClass.remove("is-invalid");
     };
-    const inValidClass = ()=>{
+    const inValidClass = () => {
         myClass.remove("is-valid");
         myClass.add("is-invalid");
     };
     switch (inputId) {
         case "nombre":
             const nombresV =
-            /^([a-zA-ZÀ-ÖØ-öø-ÿ]{3,25})([\s]?)([a-zA-ZÀ-ÖØ-öø-ÿ]{0,25})$/g;
+                /^([a-zA-ZÀ-ÖØ-öø-ÿ]{3,25})([\s]?)([a-zA-ZÀ-ÖØ-öø-ÿ]{0,25})$/g;
             formValid.nombre = myValue.match(nombresV) ? true : false;
             formValid.nombre ? validClass() : inValidClass();
             console.log(Object.values(formValid))
-        break;
+            break;
         case "correo":
             const mailV = /^([\w.]+[^#$%&\/()='"!?¡]\w*-*)([@])(\w)+(\.[a-z]{2,3})$/g;
             formValid.email = myValue.match(mailV) ? true : false;
             formValid.email ? validClass() : inValidClass();
             console.log(Object.values(formValid))
-        break;
-        case "telefono":  
-            const telefonoV = /^[0-9]*$/;  
+            break;
+        case "telefono":
+            const telefonoV = /^[0-9]*$/;
             formValid.telefono = myValue.match(telefonoV) ? true : false;
             formValid.telefono ? validClass() : inValidClass();
             console.log(Object.values(formValid))
-        break;
-        default:
-        break;
+            break;
     }
 });
